964750a673452316abb9f177468a50035e67dd71
RiteshGangwar2022 <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-02-27 01:06:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,212 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogActions from "@mui/material/DialogActions";
+import search from "./search.jpeg";
+import logo from "./logo.jpg";
+import { Typography,Box } from "@mui/material";
 
-function App() {
+const APP_ID = "95fe71db";
+const APP_KEY = "8021bc200a09add1bf28130682cd088a";
+
+const RecipeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+  width: 300px;
+  box-shadow: 0 10px 20px 0 #aaa;
+  border-radius: 10px;
+  background:#006064;
+`;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 200px;
+`;
+const RecipeName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: black;
+  margin: 10px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const SeeMoreText = styled.span`
+  color: #eb3300;
+  font-size: 18px;
+  text-align: center;
+  border: solid 1px #eb3300;
+  border-radius: 3px;
+  padding: 10px 15px;
+  cursor: pointer;
+`;
+const IngredientsText = styled(SeeMoreText)`
+  color: green;
+  border: solid 1px green;
+  margin-bottom: 12px;
+  background:#7cb342;
+  border-radius: 10px;
+`;
+const SeeNewTab = styled(SeeMoreText)`
+  color: red;
+  border: solid 1px green;
+  border-radius: 10px;
+  background:#ff8a65;
+`;
+const RecipeComponent = (props) => {
+  const [show, setShow] = useState("");
+
+  const { label, image, ingredients, url } = props.recipe;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecipeContainer>
+      <Dialog
+        onClose={() => console.log("adsadad")}
+        aria-labelledby="simple-dialog-title"
+        open={!!show}
+      
+      >
+        <DialogTitle style={{ align:"center"}}>Ingredients</DialogTitle>
+        <DialogContent>
+          <RecipeName>{label}</RecipeName>
+          <table>
+            <thead>
+              <th>Ingredient</th>
+              <th>Weight</th>
+            </thead>
+            <tbody>
+              {ingredients.map((ingredient, index) => (
+                <tr key={index} className="ingredient-list">
+                  <td>{ingredient.text}</td>
+                  <td>{ingredient.weight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <SeeNewTab onClick={() => window.open(url)}>See More</SeeNewTab>
+          <SeeMoreText onClick={() => setShow("")}>Close</SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <CoverImage src={image} alt={label} />
+      <RecipeName>{label}</RecipeName>
+      <IngredientsText onClick={() => setShow(!show)}>
+        Ingredients
+      </IngredientsText>
+      <SeeNewTab  onClick={() => window.open(url)}>
+        See Complete Recipe
+      </SeeNewTab>
+    </RecipeContainer>
   );
-}
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: #244a68;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 8px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;;
 
-export default App;
+`;
+const SearchIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+const RecipeImage = styled.img`
+  width: 90px;
+  height: 60px;
+  margin: 15px;
+  border-radius:9px;
+`;
+const Placeholder = styled.div`
+  margin:auto 30px;
+  position:relative;
+  top:100px;
+  color:white;
+
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+  width:"50%"
+`;
+
+const RecipeListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 20px;
+  gap: 20px;
+  justify-content: space-evenly;
+`;
+const AppComponent = () => {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [recipeList, updateRecipeList] = useState([]);
+  const [timeoutId, updateTimeoutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+    );
+    updateRecipeList(response.data.hits);
+  };
+
+  const onTextChange = (e) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+
+  return (
+    
+          <SearchInput
+            placeholder="Search Recipe"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      <RecipeListContainer>
+        {recipeList?.length ? (
+          recipeList.map((recipe, index) => (
+            <RecipeComponent key={index} recipe={recipe} />
+          ))
+        ) : (
+          <Placeholder>
+            <Typography variant="h1">Please Search a Recipe!</Typography>
+          </Placeholder>
+        )}
+      </RecipeListContainer>
+    </Container>
+  );
+};
+
+export default AppComponent;
